9fe4d25652a9395e9bb02bf35932b44db6664d29
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-30 22:10:41 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // function fetchTask(){
+  //   fetch("")
+  // }
+    const form = document.querySelector("form")
+    form.addEventListener("submit", event => {
+      const input = document.querySelector("input").value
+      const ul = document.querySelector("ul")
+      ul.innerHTML += `<li>${input}</li>`
+
+    })
 });
